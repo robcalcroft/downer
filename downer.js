959f9b3b2c8ba820597145da14b3cbdac4f2d4a7
robcalcroft <rob@calcroft.com>
2015-03-29 17:07:50 +0100

    Added my soundcloudr module to abstract downloading logic

--- downer.js
+++ downer.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app     = express();
 var fs      = require('fs');
 var request = require('request');
+var soundcloudr = require('soundcloudr');
 var ydl     = require('youtube-mp3');
-var apiKey  = fs.readFileSync('sc-api-key', 'utf8');
 var port    = 8002;
 
 app.use(express.static('src/'));
@@ -13,50 +13,13 @@ app.use(express.static('src/'));
  * requested soundcloud song
  */
 app.get('/api/soundcloud/download', function(req, res) {
-	var url = req.query.url;
-	var apiURL = "http://api.soundcloud.com/resolve.json?url=" + url + "&client_id=" + apiKey;
-	var streamURL;
-
-	request(apiURL, function(error, response, data) {
-
-		data = JSON.parse(data);
-
-		// Check to see if the result is a 
-		// single song or user has provided
-		// link to e.g. artist account;
-		if(data.pop) {
-			res.status(403).json({
-				status: 403,
-				message: "URL must link to a single track not an artist or other."
-			})
+	soundcloudr.setClientId(fs.readFileSync('sc-api-key', 'utf8'));
+	
+	soundcloudr.download(req.query.url, res, function(err, done) {
+		if(err) {
+			res.status(err.status).json({message: err.message})
 		}
-
-		// Check if the song is streamable
-		if(data.streamable === false) {
-			res.status(404).json({
-				status: 404,
-				message: "Track not streamable."
-			});
-			return;
-		}
-		
-		// Set headers to force download of file.
-		res.setHeader("Content-Type","application/octet-stream");
-		res.setHeader("Content-Transfer-Encoding", "Binary");
-		res.setHeader("Content-disposition", "attachment; filename=\"" + data.title + ".mp3\"");
-
-		// Create the stream URL
-		if(data.stream_url.indexOf('?') === -1) {
-			streamURL = data.stream_url + "?client_id=" +apiKey;
-		}
-		else {
-			streamURL = data.stream_url + "&client_id=" +apiKey;
-		}
-        
-		// Request the file and pipe it to
-		// the response.
-		request.get(streamURL).pipe(res);
-	})
+	});
 })
 
 app.get('/api/youtube/download', function(req, res) {
